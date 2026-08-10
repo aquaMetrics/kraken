0a9dc8d7477aa19fe7ec148aad541adabb06f617
ecodata1 <tim.foster@sepa.org.uk>
2026-08-10 15:56:48 +0100

    tests: add test for bearing calculations
    highlight differences in bearing calculation approaches for implementing new approach (mean_bearing).

--- tests/testthat/test-assess.R
+++ tests/testthat/test-assess.R
@@ -7,13 +7,10 @@ test_that("test reintraid 2020 against reported results", {
     check.names = FALSE
   )
 
-  stations <- kraken::consecutive_stations(reintraid)
-  area <- kraken::assess(reintraid)
-
-  testthat::expect_equal(round(area[[1]], 0), 35780)
-  new_area <- kraken::assess(reintraid, loess = TRUE)
-  # Current spotfire testing
-  testthat::expect_equal(round(new_area[[1]], 0), 34433)
+  area <- kraken::assess(reintraid, use_mean_bearing = FALSE)
+  # Changed on 29/06/2026 from 35780 due to using calculated lat / lon within
+  # package rather than using supplied lat / lon in test data file.
+  testthat::expect_equal(round(area[[1]], 0), 35708)
   # Result reported for compliance is slightly different (37070). Possibly due
   # to slight change to area calculator code since Feb 2022 or error in input
   # data(?). Either way, no impact on compliance (Allowable mixing zone: 86606)
@@ -40,3 +37,50 @@ test_that("test tisti-geo 2021 against reported results", {
   area <- kraken::assess(tisti, loess = FALSE, overrideTransect1 = 392)
   testthat::expect_equal(round(area[[1]], 0), 148362)
 })
+
+
+
+test_that("test bearing", {
+testthat::skip("long running test")
+  user <- tolower(Sys.info()["user"])
+ ellipse_folder <- paste0("C:/Users/",
+                          user,
+                       "/OneDrive - Scottish Environment Protection Agency/DNA Data/Readiness Data/ellipse-area/")
+ samples_path <-  paste0(ellipse_folder,  "2026-06-22-survey-iqi-data.csv")
+ survey_data <- read.csv(samples_path)
+
+ survey_data <- dplyr::select(
+   survey_data,
+   "Survey ID" = Survey.Id,
+   "Survey_date" = Survey.Date,
+   "MCFF" = Site.Id,
+   Transect,
+   "Station" = Station,
+   Easting,
+   Northing,
+   IQI = Iqi,
+   "Pen-Group" = Pen.Group,
+   Monitoring.Type
+ )
+
+ survey_data <- survey_data %>% dplyr::filter(!Transect %in% c(999, 555))
+
+
+ # 4827 survey - transect to stations duplicate station 2 (no impact on result?).
+ # 4951 survey CHA1 - transect 3 station 8 in wrong position (no impact on result?)
+ # 4815 EAM - transect station 7 in wrong position / labelled. (no impact on result?)
+ # 4981 transect 3 station 8 in wrong position / labelled. (no impact on result?)
+ output <- purrr::map_df(split(survey_data, survey_data$`Survey ID`), function(survey) {
+   survey_output <- kraken::consecutive_stations(survey)
+   survey_output <- survey_output$survey_data
+   survey_bearings <- dplyr::select(survey_output, MCFF, Survey_date, Transect,  Bearing, quickBearing, mean_bearing)
+   survey_bearings <- dplyr::distinct(survey_bearings)
+  return(survey_bearings)
+})
+ output$diff <- output$Bearing - output$quickBearing
+ output$diff_mean <- output$mean_bearing - output$quickBearing
+
+})
+
+
+
