c882b0fc5d4cf073c1c8c892b2e3bc6b253bc508
ecodata1 <tim.foster@sepa.org.uk>
2026-08-10 15:22:43 +0100

    refactor: remove rlang .data
    update linting

--- R/survey-import.R
+++ R/survey-import.R
@@ -11,11 +11,10 @@
 #' \item{label}{label - Mainly used for labelling taxonomic observations}
 #' }
 #' @export
-#' @importFrom rlang .data
 #' @examples
 #' \dontrun{
 #' file <- system.file("extdat",
-#'   "survey-template/220421-SelfMon-N4952-CAV1-Enhanced.xlsx",
+#'   "demo-data/220421-SelfMon-N4952-CAV1-Enhanced.xlsx",
 #'   package =
 #'     "kraken"
 #' )
@@ -33,12 +32,14 @@ survey_import <- function(path = NULL, spreadsheet_version = 2) {
       cover[9, 8],
       cover[c(23:26, 28:29, 31:32), 8]
     )),
-    response = unlist(c(
-      cover[10:28, 3],
-      cover[30, 3],
-      cover[10, 8],
-      cover[c(23:26, 28:29, 31:32), 9]
-    ), )
+    response = unlist(
+      c(
+        cover[10:28, 3],
+        cover[30, 3],
+        cover[10, 8],
+        cover[c(23:26, 28:29, 31:32), 9]
+      ),
+    )
   )
   survey$sample_id <- uuid::UUIDgenerate()
   project_id <- paste(survey[1, 2], survey[11, 2])
@@ -48,26 +49,27 @@ survey_import <- function(path = NULL, spreadsheet_version = 2) {
 
   transects <- rep(12, 7)
   transects <- transects * c(1, 2, 3, 4, 5, 6, 7)
-  if(spreadsheet_version == 2) {
+  if (spreadsheet_version == 2) {
     transects <- c(36, 52, 68, 84, 116, 132)
   }
   samples <- purrr::map_df(transects, function(transect) {
-    if(spreadsheet_version == 1) {
-    if (transect == 84) {
-      sample_1 <- cover[(34 + transect - 12):(34 + transect), c(1, 3:8)]
-    } else {
-      sample_1 <- cover[(34 + transect - 12):(34 + transect - 2), c(1, 3:8)]
+    if (spreadsheet_version == 1) {
+      if (transect == 84) {
+        sample_1 <- cover[(34 + transect - 12):(34 + transect), c(1, 3:8)]
+      } else {
+        sample_1 <- cover[(34 + transect - 12):(34 + transect - 2), c(1, 3:8)]
+      }
     }
-    }
-      if(spreadsheet_version == 2) {
-      sample_1 <- cover[( transect):(transect + 14), c(1, 3:8)]
+    if (spreadsheet_version == 2) {
+      sample_1 <- cover[(transect):(transect + 14), c(1, 3:8)]
     }
     names(sample_1) <- as.character(sample_1[1, ])
     sample_1 <- sample_1[2:nrow(sample_1), ]
-    sample_long <- tidyr::pivot_longer(sample_1,
-                                       cols = -1,
-                                       values_to = "response",
-                                       names_to = "sample_id"
+    sample_long <- tidyr::pivot_longer(
+      sample_1,
+      cols = -1,
+      values_to = "response",
+      names_to = "sample_id"
     )
 
     names(sample_long) <- c("question", "sample_id", "response")
@@ -102,16 +104,17 @@ survey_import <- function(path = NULL, spreadsheet_version = 2) {
   )
   # Loop through sheets and format
   sheets <- readxl::excel_sheets(path)
-  data_sheets_ref <- data_sheets_ref[data_sheets_ref %in% sheets ]
+  data_sheets_ref <- data_sheets_ref[data_sheets_ref %in% sheets]
   data_sheets <- purrr::map_df(data_sheets_ref, function(data_sheet) {
     t1_data <- suppressMessages(readxl::read_xlsx(path, sheet = data_sheet))
     names(t1_data) <- as.character(t1_data[2, ])
     t1_data <- t1_data[3:nrow(t1_data), ]
     t1_data <- dplyr::select(t1_data, -"NA")
-    t1_data <- tidyr::pivot_longer(t1_data,
-                                   cols = c(2:ncol(t1_data)),
-                                   names_to = "sample_id",
-                                   values_to = "response"
+    t1_data <- tidyr::pivot_longer(
+      t1_data,
+      cols = c(2:ncol(t1_data)),
+      names_to = "sample_id",
+      values_to = "response"
     )
     t1_data <- dplyr::select(t1_data, "sample_id", question = 1, "response")
     t1_data <- dplyr::filter(t1_data, !is.na(question))
@@ -134,63 +137,68 @@ survey_import <- function(path = NULL, spreadsheet_version = 2) {
   )
   # Loop through fauna worksheets and format data
   sheets <- readxl::excel_sheets(path)
-  fauna_sheets_ref <- fauna_sheets_ref[fauna_sheets_ref %in% sheets ]
+  fauna_sheets_ref <- fauna_sheets_ref[fauna_sheets_ref %in% sheets]
   fauna_sheets <- purrr::map_df(fauna_sheets_ref, function(data_sheet) {
     fauna <- suppressMessages(readxl::read_xlsx(path, sheet = data_sheet))
     # Format 'number of replicates' info
     replicates <- fauna[2, ]
-    replicates <- tidyr::pivot_longer(replicates,
-                                      cols = c(4:ncol(fauna)),
-                                      names_to = "sample_id",
-                                      values_to = "response"
+    replicates <- tidyr::pivot_longer(
+      replicates,
+      cols = c(4:ncol(fauna)),
+      names_to = "sample_id",
+      values_to = "response"
     )
     replicates$project_id <- project_id
-    replicates <- dplyr::select(replicates,
-                                "project_id",
-                                "sample_id",
-                                question = "...3",
-                                "response"
+    replicates <- dplyr::select(
+      replicates,
+      "project_id",
+      "sample_id",
+      question = "...3",
+      "response"
     )
 
     # Format taxonomic info
     fauna <- fauna[4:nrow(fauna), ]
-    fauna <- tidyr::pivot_longer(fauna,
-                                 cols = c(4:ncol(fauna)),
-                                 names_to = "sample_id",
-                                 values_to = "response"
+    fauna <- tidyr::pivot_longer(
+      fauna,
+      cols = c(4:ncol(fauna)),
+      names_to = "sample_id",
+      values_to = "response"
     )
-    fauna <- tidyr::pivot_longer(fauna,
-                                 cols = -c(2, 4)
-    )
+    fauna <- tidyr::pivot_longer(fauna, cols = -c(2, 4))
 
-    fauna <- dplyr::select(fauna,
-                           "sample_id",
-                           "question" = "name",
-                           "response" = "value",
-                           "label" = 1,
+    fauna <- dplyr::select(
+      fauna,
+      "sample_id",
+      "question" = "name",
+      "response" = "value",
+      "label" = 1,
     )
 
     # Define 'question' for comment, count and MCS responses
-    fauna <- dplyr::mutate(fauna,
-                           question = replace(
-                             question,
-                             question == "...3",
-                             "comment"
-                           )
+    fauna <- dplyr::mutate(
+      fauna,
+      question = replace(
+        question,
+        question == "...3",
+        "comment"
+      )
     )
-    fauna <- dplyr::mutate(fauna,
-                           question =
-                             replace(
-                               question,
-                               question == "Transect 1 Species Abundance Matrix",
-                               "MCS Code"
-                             )
+    fauna <- dplyr::mutate(
+      fauna,
+      question = replace(
+        question,
+        question == "Transect 1 Species Abundance Matrix",
+        "MCS Code"
+      )
     )
-    fauna <- dplyr::mutate(fauna,
-                           question = replace(
-                             question, question == "response",
-                             "Taxon abundance"
-                           )
+    fauna <- dplyr::mutate(
+      fauna,
+      question = replace(
+        question,
+        question == "response",
+        "Taxon abundance"
+      )
     )
     fauna <- dplyr::filter(fauna, !is.na(label))
     fauna$project_id <- project_id
